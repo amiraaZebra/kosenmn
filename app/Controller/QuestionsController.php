7444526203bc0ea014d495363b527aa72974468d
amiraaZebra <amraa_ax@yahoo.com>
2014-11-15 20:45:17 +0900

    contuct us page nees sanal huselt bichuulj avah table nemsen table name: questions sqlcode: app folder dotor kosenmn_questions_2014_11_15.sql geed bga. DB de table ee nemeerei

---
+++ app/Controller/QuestionsController.php
@@ -0,0 +1,89 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * Questions Controller
+ *
+ * @property Question $Question
+ * @property PaginatorComponent $Paginator
+ */
+class QuestionsController extends AppController {
+
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->Question->recursive = 0;
+		$this->set('questions', $this->Paginator->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Question->exists($id)) {
+			throw new NotFoundException(__('Invalid question'));
+		}
+		$options = array('conditions' => array('Question.' . $this->Question->primaryKey => $id));
+		$this->set('question', $this->Question->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+//			$this->request->data['Question']['ip_address']=$this->request->clientIp();
+			$this->Question->create();
+			if ($this->Question->save($this->request->data)) {
+				$this->Session->setFlash(__('The question has been saved.'));
+				return $this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The question could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Question->id = $id;
+		if (!$this->Question->exists()) {
+			throw new NotFoundException(__('Invalid question'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Question->delete()) {
+			$this->Session->setFlash(__('The question has been deleted.'));
+		} else {
+			$this->Session->setFlash(__('The question could not be deleted. Please, try again.'));
+		}
+		return $this->redirect(array('action' => 'index'));
+	}
+		public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow();		
+	}	
+	
+	public function isAuthorized($user = null) {
+		return true;
+	}
+}
